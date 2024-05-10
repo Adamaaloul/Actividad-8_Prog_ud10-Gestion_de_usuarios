607316ca038bfeab3956722254134c873dbb517e
Adam <adamakloul@gmail.com>
2024-05-10 18:12:19 +0200

    Primer commit

---
+++ src/main/java/es/progcipfpbatoi/progud10classwork/Usuario.java
@@ -0,0 +1,10 @@
+package es.progcipfpbatoi.progud10classwork;
+
+public class Usuario {
+	
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	private 
+	
+}
---
+++ src/test/java/es/progcipfpbatoi/progud10classwork/ProgUd10ClassworkA8GestionDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package es.progcipfpbatoi.progud10classwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgUd10ClassworkA8GestionDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
